aa0e3c08a5540c8244a027bbdbcd80171a544095
Saikumar <marjunsaikumar@gmail.com>
2023-06-06 13:33:45 +0000

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,36 @@
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-container">
+    <div className="wave-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+        alt="wave"
+        className="wave-img"
+      />
+      <h1 className="wave-text">Wave</h1>
+    </div>
+
+    <ul className="nav-menu">
+      <li>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/contact">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+export default Header
